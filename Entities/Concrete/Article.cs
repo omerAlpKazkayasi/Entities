6a86ecc496a2e6ac4571a15d92e95e1afe663a2d
qwe_c <qwe_c@DESKTOP-QCO3J68>
2023-11-22 14:11:59 +0300

    Add project files.

---
+++ Entities/Concrete/Article.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Article:BaseEntity
+    {
+        public List<CategoryArticle> ArticleCategories { get; set; }
+        public string Title { get; set; }        
+        public string Content { get; set; }
+        public int ReadingTime { get; set; }
+        public int ReadingCount { get; set; }
+        public AppUser User { get; set; }
+        public string UserId { get; set; }
+
+    }
+    public class Category:BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public List<UserCategory> Users { get; set; }
+        public List<CategoryArticle> ArticleCategories { get; set; }
+    }
+    
+    public class UserCategory:BaseEntity
+    {
+        public int CategoryID { get; set;}
+        public Category Category { get; set; }
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+    }
+    public class AppUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? UserUrl { get; set; }
+        public List<UserCategory> UserCategory { get; set; }
+        public List<Article> Article { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/CategoryArticle.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Concrete
+{
+    public class CategoryArticle:BaseEntity
+    {
+        public Article Article   { get; set; }
+        public int   ArticleID     { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
